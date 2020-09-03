8994ac913b1643d0f1053f2d97f4671b9340389c
Maria <makasyamka@gmail.com>
2020-09-03 15:54:55 +0300

    Add StarButton component

---
+++ src/StarButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="toogleFavorite(movie.id)" :class="{'is-favorite': isFavorite}" class="star-button">&#9733;</button>
+</template>
+
+<script>
+import { eventBus } from "./eventBus.js";
+import StarButton from "./StarButton.vue";
+
+export default {
+  name: "star-button",
+
+  props: {
+    movie: { type: Object },
+  },
+
+  data() {
+    return {
+      isFavorite: this.movie.isFavorite,
+    };
+  },
+
+  methods: {
+    toogleFavorite(id) {
+      this.isFavorite = !this.isFavorite;
+      eventBus.$emit("toogle-favorite", this.isFavorite, id);
+    },
+  },
+};
+</script>
